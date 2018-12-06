1c26f112e578c0e6ec2c40bfffe24e2452e730f2
Patrik Varga <patrikadamvarga@gmail.com>
2018-12-06 18:45:06 +0100

    Textboxokat cseréltem labelre

--- AFP_2018_C/AFP_2018_C/FormSzuloSzerepkor.Designer.cs
+++ AFP_2018_C/AFP_2018_C/FormSzuloSzerepkor.Designer.cs
@@ -30,8 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSzuloSzerepkor));
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -44,15 +44,6 @@
             this.button1.Text = "eredmények";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(96, 46);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(112, 53);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "Szülő";
-            // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(12, 12);
@@ -63,14 +54,24 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(121, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 39);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Szülő";
+            // 
             // FormSzuloSzerepkor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(780, 521);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "FormSzuloSzerepkor";
             this.Text = "FormSzuloSzerepkor";
@@ -82,7 +83,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
