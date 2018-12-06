66714c488fb9a0b2170b5f023498d04914d538a6
gdavid06 <slifer252@gmail.com>
2018-12-06 18:46:46 +0100

    textboxokat cseréltem labelre(feliratoknál)

--- AFP_2018_C/AFP_2018_C/FormSzuloSzerepkor.Designer.cs
+++ AFP_2018_C/AFP_2018_C/FormSzuloSzerepkor.Designer.cs
@@ -58,7 +58,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(121, 83);
+            this.label1.Location = new System.Drawing.Point(116, 74);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(103, 39);
             this.label1.TabIndex = 3;
