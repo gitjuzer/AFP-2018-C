d8efb73e6396fae245f9992f4705504c72c49ca4
tremlferenc <02feri02@gmail.com>
2018-12-12 21:45:25 +0100

    setterek hozzaadva

--- AFP_2018_C/AFP_2018_C/User.cs
+++ AFP_2018_C/AFP_2018_C/User.cs
@@ -14,17 +14,19 @@ namespace AFP_2018_C
         public string Username
         {
             get { return username; }
+            set { username = value; }
         }
 
         public string Password
         {
             get { return password; }
+            set { password = value; }
         }
 
         public User(string username, string password)
         {
-            this.username = username;
-            this.password = password;
+            Username = username;
+            Password = password;
         }
     }
 }
