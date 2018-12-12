dedc10cc2b286b55f48dd02eb1813ad49114ec6e
tremlferenc <02feri02@gmail.com>
2018-12-12 22:38:13 +0100

    egyszeru teszt a username propertyre

---
+++ AFP_2018_C/AFP_2018_CTests/UsersClasses/UserTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AFP_2018_C;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFP_2018_C.Tests
+{
+    [TestClass()]
+    public class UserTests
+    {
+        [TestMethod()]
+        public void UserTestUserUsername1()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ AFP_2018_C/AFP_2018_CTests1/UsersClasses/UserTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AFP_2018_C;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFP_2018_C.Tests
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void UserTestUserUsername1()
+        {
+            string username = "username";
+            User user = new User(username, "password");
+
+            string expected = "username";
+
+            string actual = user.Username;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        
+    }
+}
