f936cd5bfa33b9f2ced5e416b145afceab373b78
patriksalamon <43856787+patriksalamon@users.noreply.github.com>
2018-12-12 21:14:48 +0100

    Tanuló Témakörök megadása
    ahhoz formok létrehozása

--- AFP_2018_C/AFP_2018_C/FormTanuloSzerepkor.Designer.cs
+++ AFP_2018_C/AFP_2018_C/FormTanuloSzerepkor.Designer.cs
@@ -35,9 +35,6 @@
             this.radioButton7 = new System.Windows.Forms.RadioButton();
             this.radioButton8 = new System.Windows.Forms.RadioButton();
             this.radioButton9 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
-            this.radioButton6 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
@@ -52,10 +49,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(735, 54);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(551, 44);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(148, 48);
+            this.label1.Size = new System.Drawing.Size(122, 39);
             this.label1.TabIndex = 1;
             this.label1.Text = "Tanuló";
             // 
@@ -63,29 +59,24 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(16, 322);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(12, 262);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(216, 29);
+            this.label2.Size = new System.Drawing.Size(168, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "Válassz témakört!";
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.radioButton7);
+            this.groupBox1.Controls.Add(this.buttonTovabbTemakorhoz);
             this.groupBox1.Controls.Add(this.radioButton8);
             this.groupBox1.Controls.Add(this.radioButton9);
-            this.groupBox1.Controls.Add(this.radioButton4);
-            this.groupBox1.Controls.Add(this.radioButton5);
-            this.groupBox1.Controls.Add(this.radioButton6);
             this.groupBox1.Controls.Add(this.radioButton3);
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(23, 374);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Location = new System.Drawing.Point(17, 304);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(544, 252);
+            this.groupBox1.Size = new System.Drawing.Size(408, 205);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Témakörök";
@@ -94,146 +85,99 @@
             // 
             this.radioButton7.AutoSize = true;
             this.radioButton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.radioButton7.Location = new System.Drawing.Point(385, 42);
-            this.radioButton7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton7.Location = new System.Drawing.Point(183, 34);
             this.radioButton7.Name = "radioButton7";
-            this.radioButton7.Size = new System.Drawing.Size(104, 24);
+            this.radioButton7.Size = new System.Drawing.Size(104, 21);
             this.radioButton7.TabIndex = 8;
             this.radioButton7.TabStop = true;
-            this.radioButton7.Text = "Temakor1";
+            this.radioButton7.Text = "Német nyelv";
             this.radioButton7.UseVisualStyleBackColor = true;
             // 
             // radioButton8
             // 
             this.radioButton8.AutoSize = true;
             this.radioButton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.radioButton8.Location = new System.Drawing.Point(385, 103);
-            this.radioButton8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton8.Location = new System.Drawing.Point(183, 84);
             this.radioButton8.Name = "radioButton8";
-            this.radioButton8.Size = new System.Drawing.Size(104, 24);
+            this.radioButton8.Size = new System.Drawing.Size(136, 21);
             this.radioButton8.TabIndex = 7;
             this.radioButton8.TabStop = true;
-            this.radioButton8.Text = "Temakor1";
+            this.radioButton8.Text = "Környezetismeret";
             this.radioButton8.UseVisualStyleBackColor = true;
             // 
             // radioButton9
             // 
             this.radioButton9.AutoSize = true;
             this.radioButton9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.radioButton9.Location = new System.Drawing.Point(385, 167);
-            this.radioButton9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton9.Location = new System.Drawing.Point(183, 136);
             this.radioButton9.Name = "radioButton9";
-            this.radioButton9.Size = new System.Drawing.Size(104, 24);
+            this.radioButton9.Size = new System.Drawing.Size(80, 21);
             this.radioButton9.TabIndex = 6;
             this.radioButton9.TabStop = true;
-            this.radioButton9.Text = "Temakor1";
+            this.radioButton9.Text = "Irodalom";
             this.radioButton9.UseVisualStyleBackColor = true;
-            // 
-            // radioButton4
-            // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.radioButton4.Location = new System.Drawing.Point(213, 42);
-            this.radioButton4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(104, 24);
-            this.radioButton4.TabIndex = 5;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "Temakor1";
-            this.radioButton4.UseVisualStyleBackColor = true;
-            // 
-            // radioButton5
-            // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.radioButton5.Location = new System.Drawing.Point(213, 103);
-            this.radioButton5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(104, 24);
-            this.radioButton5.TabIndex = 4;
-            this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "Temakor1";
-            this.radioButton5.UseVisualStyleBackColor = true;
-            // 
-            // radioButton6
-            // 
-            this.radioButton6.AutoSize = true;
-            this.radioButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.radioButton6.Location = new System.Drawing.Point(213, 167);
-            this.radioButton6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(104, 24);
-            this.radioButton6.TabIndex = 3;
-            this.radioButton6.TabStop = true;
-            this.radioButton6.Text = "Temakor1";
-            this.radioButton6.UseVisualStyleBackColor = true;
             // 
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
             this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.radioButton3.Location = new System.Drawing.Point(39, 167);
-            this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton3.Location = new System.Drawing.Point(29, 136);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(104, 24);
+            this.radioButton3.Size = new System.Drawing.Size(99, 21);
             this.radioButton3.TabIndex = 2;
             this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Temakor1";
+            this.radioButton3.Text = "Angol nyelv";
             this.radioButton3.UseVisualStyleBackColor = true;
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.radioButton2.Location = new System.Drawing.Point(39, 103);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton2.Location = new System.Drawing.Point(29, 84);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(104, 24);
+            this.radioButton2.Size = new System.Drawing.Size(95, 21);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Temakor1";
+            this.radioButton2.Text = "Informatika";
             this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.radioButton1.Location = new System.Drawing.Point(39, 42);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton1.Location = new System.Drawing.Point(29, 34);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(104, 24);
+            this.radioButton1.Size = new System.Drawing.Size(98, 21);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Temakor1";
+            this.radioButton1.Text = "Matematika";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // buttonTovabbTemakorhoz
             // 
-            this.buttonTovabbTemakorhoz.Location = new System.Drawing.Point(804, 466);
-            this.buttonTovabbTemakorhoz.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonTovabbTemakorhoz.Location = new System.Drawing.Point(313, 149);
             this.buttonTovabbTemakorhoz.Name = "buttonTovabbTemakorhoz";
-            this.buttonTovabbTemakorhoz.Size = new System.Drawing.Size(119, 47);
+            this.buttonTovabbTemakorhoz.Size = new System.Drawing.Size(89, 38);
             this.buttonTovabbTemakorhoz.TabIndex = 4;
-            this.buttonTovabbTemakorhoz.Text = "Tovább a témakörökhöz";
+            this.buttonTovabbTemakorhoz.Text = "Tovább a témakörhöz";
             this.buttonTovabbTemakorhoz.UseVisualStyleBackColor = true;
+            this.buttonTovabbTemakorhoz.Click += new System.EventHandler(this.buttonTovabbTemakorhoz_Click_1);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-691, -1);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(-518, -1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1936, 647);
+            this.pictureBox1.Size = new System.Drawing.Size(1452, 526);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1101, 15);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(826, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
             this.button1.Text = "Vissza";
             this.button1.UseVisualStyleBackColor = true;
@@ -241,16 +185,14 @@
             // 
             // FormTanuloSzerepkor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1217, 641);
+            this.ClientSize = new System.Drawing.Size(913, 521);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.buttonTovabbTemakorhoz);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormTanuloSzerepkor";
             this.Text = "Tanuló szerepkör választás";
             this.groupBox1.ResumeLayout(false);
@@ -271,9 +213,6 @@
         private System.Windows.Forms.RadioButton radioButton7;
         private System.Windows.Forms.RadioButton radioButton8;
         private System.Windows.Forms.RadioButton radioButton9;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.RadioButton radioButton5;
-        private System.Windows.Forms.RadioButton radioButton6;
         private System.Windows.Forms.Button buttonTovabbTemakorhoz;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
--- AFP_2018_C/AFP_2018_C/FormTanuloSzerepkor.cs
+++ AFP_2018_C/AFP_2018_C/FormTanuloSzerepkor.cs
@@ -25,6 +25,8 @@ namespace AFP_2018_C
         private void buttonTovabbTemakorhoz_Click(object sender, EventArgs e)
         {
 
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -44,6 +46,67 @@ namespace AFP_2018_C
         private void button_kilepes_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void buttonTovabbTemakorhoz_Click_1(object sender, EventArgs e)
+        {
+
+            if(radioButton1.Checked)
+            {
+                FormTanuloMatek formTanuloMatek = new FormTanuloMatek();
+                this.Hide();
+                formTanuloMatek.ShowDialog();
+                this.Close();
+            }
+            else if(radioButton2.Checked)
+            {
+                FormTanuloInformatika formTanuloInformatika = new FormTanuloInformatika();
+                this.Hide();
+                formTanuloInformatika.ShowDialog();
+                this.Close();
+
+            }
+            else if(radioButton3.Checked)
+            {
+                FormTanuloAngol formTanuloAngol = new FormTanuloAngol();
+                this.Hide();
+                formTanuloAngol.ShowDialog();
+                this.Close();
+
+
+            }
+            else if (radioButton7.Checked)
+            {
+                FormTanuloNemet formTanuloNemet = new FormTanuloNemet();
+                this.Hide();
+                formTanuloNemet.ShowDialog();
+                this.Close();
+
+
+            }
+            else if (radioButton8.Checked)
+            {
+                FormTanuloKornyezetismeret  formTanuloKornyezetismeret = new FormTanuloKornyezetismeret();
+                this.Hide();
+                formTanuloKornyezetismeret.ShowDialog();
+                this.Close();
+            }
+            else if (radioButton9.Checked)
+            {
+                FormTanuloIrodalom formTanuloIrodalom = new FormTanuloIrodalom();
+                this.Hide();
+                formTanuloIrodalom.ShowDialog();
+                this.Close();
+
+
+            }
+            else
+            {
+                MessageBox.Show("Témakör választása kötelező!");
+
+            }
+
+
         }
     }
 }
