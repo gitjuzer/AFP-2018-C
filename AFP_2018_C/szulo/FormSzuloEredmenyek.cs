4cf7e755d2fb4b4b555a33dcfb5c5768306547f5
patriksalamon <43856787+patriksalamon@users.noreply.github.com>
2018-12-28 13:39:28 +0100

    Szulo Admin-e Unit Teszt

---
+++ AFP_2018_C/szulo/FormSzuloEredmenyek.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AFP_2018_C
+{
+    public partial class FormSzuloEredmenyek : Form
+    {
+        
+        public FormSzuloEredmenyek()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var myForm = new FormSzuloSzerepkor();
+            this.Hide();
+            myForm.ShowDialog();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AFP_2018_C/szulo/LoginSzulo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AFP_2018_C
+{
+    public partial class LoginSzulo : Form
+    {
+
+        public Szulo szulo { get; set; }
+
+        string szerepkor = FormSzerepkor.szerepkor;
+        public LoginSzulo()
+        {
+            InitializeComponent();
+            textBox_password.PasswordChar = '*';
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            
+            label3.Text = "";
+            if(textBox_username.Text =="admin" && textBox_password.Text == "admin")
+            {
+                    FormSzuloSzerepkor formSzuloSzerepkor = new FormSzuloSzerepkor();
+                    this.Hide();
+                    formSzuloSzerepkor.ShowDialog();
+                    this.Close();
+                szulo.IsAdmin = true;
+             
+
+            }
+            label3.Text = "Sikertelen bejelentkezés!";
+            textBox_password.Text = "";
+            textBox_username.Text = "";
+            szulo.IsAdmin = false;
+
+        }
+    }
+
+    public class Szulo
+    {
+        public bool IsAdmin { get; set; }
+
+    }
+
+
+}
