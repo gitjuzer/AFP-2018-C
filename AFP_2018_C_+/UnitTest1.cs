c18523824a9deae637e16bb05e64f5204f16b21f
patriksalamon <43856787+patriksalamon@users.noreply.github.com>
2018-12-28 13:32:31 +0100

    Szulo Admin-e Unit teszt

---
+++ AFP_2018_C_+/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AFP_2018_C;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AFP_2018_C__
+{
+    [TestClass]
+    public class SzuloAdminTest
+    {
+        [TestMethod]
+        public void SzuloAdmine()
+        {
+            var loginSzulo = new LoginSzulo();
+
+            var result = loginSzulo.szulo.IsAdmin;
+
+            Assert.IsTrue(result);
+
+
+            }
+        }
+  
+}
