13eb6a1a3268f6ea29d37ae6d459cbda0496b78f
SulyokTams <sulyoktams@gmail.com>
2018-12-31 19:17:31 +0100

    Messenger Admin - Szülő szoba elkészítése.

--- AFP_2018_C/AFP_2018_C/szulo/FormSzuloMessengerAdminSzulo.Designer.cs
+++ AFP_2018_C/AFP_2018_C/szulo/FormSzuloMessengerAdminSzulo.Designer.cs
@@ -28,19 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSzuloMessengerAdminSzulo));
+            this.Messenger = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Chat = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Szoveg = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // Messenger
+            // 
+            this.Messenger.AutoSize = true;
+            this.Messenger.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.Messenger.Location = new System.Drawing.Point(35, 28);
+            this.Messenger.Name = "Messenger";
+            this.Messenger.Size = new System.Drawing.Size(79, 16);
+            this.Messenger.TabIndex = 14;
+            this.Messenger.Text = "Messenger:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 339);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 35);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Küldés";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(38, 339);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 35);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Vissza";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Chat
+            // 
+            this.Chat.FormattingEnabled = true;
+            this.Chat.Location = new System.Drawing.Point(38, 56);
+            this.Chat.Name = "Chat";
+            this.Chat.Size = new System.Drawing.Size(391, 173);
+            this.Chat.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.label1.Location = new System.Drawing.Point(35, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Üzeneted:";
+            // 
+            // Szoveg
+            // 
+            this.Szoveg.Location = new System.Drawing.Point(38, 273);
+            this.Szoveg.Name = "Szoveg";
+            this.Szoveg.Size = new System.Drawing.Size(391, 20);
+            this.Szoveg.TabIndex = 22;
             // 
             // FormSzuloMessengerAdminSzulo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(467, 409);
+            this.Controls.Add(this.Szoveg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Chat);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Messenger);
             this.Name = "FormSzuloMessengerAdminSzulo";
             this.Text = "Admin - Szülő szoba";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Messenger;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox Chat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Szoveg;
     }
 }
